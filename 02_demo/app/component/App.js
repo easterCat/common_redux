9c6fd44bbc1141a57f00462f710d571069a236ab
fuhuodemao@163.com <fuhuodemao@163.com>
2017-10-09 18:09:45 +0800

    1.0.1

---
+++ 02_demo/app/component/App.js
@@ -0,0 +1,12 @@
+/**
+ * Created by easterCat on 2017/10/9.
+ */
+import React from 'react';
+
+function App() {
+    return (
+        <div>hello world</div>
+    )
+}
+
+export default App;
---
+++ 02_demo/webpack.config.js
@@ -0,0 +1,12 @@
+/**
+ * Created by easterCat on 2017/10/9.
+ */
+const config = {
+    entry: './app/index.js',
+    output: {},
+    module: {},
+    devServer: {},
+    plugins: {}
+};
+
+module.exports = config;
